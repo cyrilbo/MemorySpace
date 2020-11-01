9a6d9e83534b9b081451410431da7bc94f24952f
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-01 14:58:51 +0100

    config react-navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React, { FunctionComponent } from 'react';
 import { SafeAreaView, StatusBar, Text } from 'react-native';
 
 const App: FunctionComponent = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Text>MemorySpace</Text>
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
 
