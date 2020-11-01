f478e135cc422127194f387d6a6702f114b23a7c
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-01 14:12:28 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, StatusBar} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>MemorySpace</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
