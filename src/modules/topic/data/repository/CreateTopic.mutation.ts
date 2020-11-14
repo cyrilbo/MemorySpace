a81ea70166a96bdd47604b3f2683cfb501309be9
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-14 12:46:01 +0100

    fix createTopic mutation

--- src/modules/topic/data/repository/CreateTopic.mutation.ts
+++ src/modules/topic/data/repository/CreateTopic.mutation.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm/browser';
 import { Topic } from '../../types/Topic.type';
+import { getRandomTopicColor } from './../../utils/getRandomTopicColor.utils';
 import { adaptTopicEntityToTopic } from './Topic.adapters';
 import { TopicEntity } from './Topic.entity';
 
@@ -7,6 +8,7 @@ export const createTopic = async (name: string): Promise<Topic> => {
   const topicRepository = getRepository(TopicEntity);
   const newTopicEntity = new TopicEntity();
   newTopicEntity.name = name;
+  newTopicEntity.colorId = getRandomTopicColor().id;
   const topicEntityCreated = await topicRepository.save(newTopicEntity);
   return adaptTopicEntityToTopic(topicEntityCreated);
 };
