86493e217a54504e47a122f2fd95e592bffe932e
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-14 21:18:03 +0100

    add header in card navigator

--- src/navigation/CardNavigator/CardNavigator.navigator.tsx
+++ src/navigation/CardNavigator/CardNavigator.navigator.tsx
@@ -1,6 +1,7 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React, { FunctionComponent } from 'react';
 import { StatusBar } from 'react-native';
+import { colors } from '../../core/theme/colors';
 import { TopicCardList } from '../../pages/TopicCardList/TopicCardList.page';
 import { CardNavigatorRouteNames, CardNavigatorRouteParamsList } from './CardNavigator.routes';
 
@@ -9,8 +10,22 @@ const Stack = createStackNavigator<CardNavigatorRouteParamsList>();
 export const CardNavigator: FunctionComponent = () => (
   <>
     <StatusBar barStyle="dark-content" />
-    <Stack.Navigator mode="modal" headerMode="none">
-      <Stack.Screen name={CardNavigatorRouteNames.TopicCardList} component={TopicCardList} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.darkGrey,
+        },
+        headerTintColor: colors.ivory,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen
+        name={CardNavigatorRouteNames.TopicCardList}
+        component={TopicCardList}
+        options={({ route }) => ({ headerTitle: route.params.topic.name })}
+      />
     </Stack.Navigator>
   </>
 );
