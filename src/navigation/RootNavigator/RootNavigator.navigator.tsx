f46fc057a9b74620408f527fd70786c911a0b8f6
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-14 18:28:35 +0100

    fix statusBar style

--- src/navigation/RootNavigator/RootNavigator.navigator.tsx
+++ src/navigation/RootNavigator/RootNavigator.navigator.tsx
@@ -1,6 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React, { FunctionComponent } from 'react';
+import { StatusBar } from 'react-native';
+import { colors } from '../../core/theme/colors';
 import { CreateCardModal } from '../../pages/CreateCardModal/CreateCardModal.page';
 import { Home } from '../../pages/Home/Home.page';
 import { SelectTopicModal } from '../../pages/SelectTopicModal/SelectTopicModal.page';
@@ -10,6 +12,7 @@ const Stack = createStackNavigator<RootNavigatorRouteParamsList>();
 
 export const RootNavigator: FunctionComponent = () => (
   <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor={colors.darkGrey} />
     <Stack.Navigator mode="modal" headerMode="none">
       <Stack.Screen name={RootNavigatorRouteNames.Home} component={Home} />
       <Stack.Screen name={RootNavigatorRouteNames.CreateCardModal} component={CreateCardModal} />
