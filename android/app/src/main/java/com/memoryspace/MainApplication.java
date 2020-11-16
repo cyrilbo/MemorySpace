2bdb013b2b25e0d4610ebfb8928d8f2762800489
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-16 21:06:40 +0100

    setup codepush android

--- android/app/src/main/java/com/memoryspace/MainApplication.java
+++ android/app/src/main/java/com/memoryspace/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -33,6 +34,14 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+        // the CodePush runtime determine where to get the JS
+        // bundle location from on each app start
+        @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
+
       };
 
   @Override
