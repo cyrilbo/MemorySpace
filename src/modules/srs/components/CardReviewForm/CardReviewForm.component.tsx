5bee267ae198e2101d6ba98f72bc376daf43f2ba
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-22 10:53:30 +0100

    remove blur because of performance issues on android

--- src/modules/srs/components/CardReviewForm/CardReviewForm.component.tsx
+++ src/modules/srs/components/CardReviewForm/CardReviewForm.component.tsx
@@ -1,38 +1,30 @@
-import { BlurView } from '@react-native-community/blur';
 import React, { FunctionComponent, useState } from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Spacer } from '../../../../core/components/Spacer/Spacer.component';
 import { EyeIcon } from '../../../../core/icons/Eye/EyeIcon.component';
-import { theme } from '../../../../core/theme';
 import { colors } from '../../../../core/theme/colors';
 import styled from '../../../../core/theme/styled-components';
 import { useSubmitCardReviewMutation } from '../../../card/data/hooks/useSubmitCardReviewMutation.hook';
 import { Card } from '../../../card/types/Card.type';
-import { getHexFromTopicColorId } from '../../../topic/utils/getHexFromTopicColorId.utils';
 import { ResultForm } from '../ResultForm/ResultForm.component';
 
 interface Props {
   card: Card;
 }
 
-const MARGIN_BOTTOM = 4;
-
 export const CardReviewForm: FunctionComponent<Props> = ({ card }) => {
   const [isAnswerVisible, setIsAnswerVisible] = useState(false);
   const { submitCardReview } = useSubmitCardReviewMutation(() => setIsAnswerVisible(false));
-  const insets = useSafeAreaInsets();
-
   return (
     <Container>
       <QuestionContainer>
         <Question>{card.question}</Question>
       </QuestionContainer>
-      <AnswerContainer>
-        <Answer>{card.answer}</Answer>
-      </AnswerContainer>
 
       {isAnswerVisible ? (
-        <ResultFormContainer marginBottom={insets.bottom + MARGIN_BOTTOM * theme.gridUnit}>
+        <>
+          <AnswerContainer>
+            <Answer>{card.answer}</Answer>
+          </AnswerContainer>
           <ResultForm
             onFailurePress={() => {
               submitCardReview({ cardId: card.id, isCardWellKnown: false });
@@ -41,64 +33,51 @@ export const CardReviewForm: FunctionComponent<Props> = ({ card }) => {
               submitCardReview({ cardId: card.id, isCardWellKnown: true });
             }}
           />
-        </ResultFormContainer>
-      ) : null}
-      <Spacer height={insets.bottom / theme.dividerSize + MARGIN_BOTTOM} />
-      {!isAnswerVisible ? (
-        <Blur
-          blurType="light"
-          blurAmount={2}
-          reducedTransparencyFallbackColor={getHexFromTopicColorId(card.topic.colorId)}
+          <Spacer height={10} />
+        </>
+      ) : (
+        <EyeIconContainer
+          onPress={() => {
+            setIsAnswerVisible(!isAnswerVisible);
+          }}
         >
-          <EyeIconContainer
-            onPress={() => {
-              setIsAnswerVisible(!isAnswerVisible);
-            }}
-          >
-            <EyeIcon color={colors.black} size={30} />
-          </EyeIconContainer>
-        </Blur>
-      ) : null}
+          <EyeIcon color={colors.black} size={30} />
+        </EyeIconContainer>
+      )}
     </Container>
   );
 };
 
-const Container = styled.View({
+const Container = styled.View(({ theme }) => ({
   flex: 1,
   justifyContent: 'center',
-  paddingHorizontal: theme.gridUnit * 4,
-});
+  paddingVertical: theme.gridUnit * 5,
+}));
 
-const QuestionContainer = styled.View({ flex: 1, justifyContent: 'center', zIndex: 10 });
+const QuestionContainer = styled.View(({ theme }) => ({
+  justifyContent: 'center',
+  borderTopWidth: 4,
+  borderBottomWidth: 4,
+  padding: theme.gridUnit * 5,
+}));
 
-const Question = styled.Text({
+const Question = styled.Text(({ theme }) => ({
   fontFamily: theme.fontFamilies.bold,
   fontSize: theme.fontSizes.xxxl,
-});
+}));
 
-const AnswerContainer = styled.ScrollView({ flex: 1 });
-const Answer = styled.Text({
+const AnswerContainer = styled.ScrollView(({ theme }) => ({
+  flex: 1,
+  padding: theme.gridUnit * 5,
+}));
+
+const Answer = styled.Text(({ theme }) => ({
   fontFamily: theme.fontFamilies.regular,
-  fontSize: theme.fontSizes.l,
-});
-
-const Blur = styled(BlurView)({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  bottom: 0,
-  right: 0,
-});
+  fontSize: theme.fontSizes.xl,
+}));
 
 const EyeIconContainer = styled.TouchableOpacity({
   flex: 1,
   alignItems: 'center',
   justifyContent: 'center',
 });
-
-const ResultFormContainer = styled.View<{ marginBottom: number }>(({ marginBottom }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  bottom: marginBottom,
-}));
