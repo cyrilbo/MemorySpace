ac78f624f041c7d86166f6bfe7a37abd7b253e4b
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-22 09:48:29 +0100

    add Montserrat font

---
+++ src/core/theme/fontSizes.ts
@@ -0,0 +1,8 @@
+export const fontSizes = {
+  s: 11,
+  m: 12,
+  l: 14,
+  xl: 16,
+  xxl: 18,
+  xxxl: 20,
+};
--- src/core/theme/index.ts
+++ src/core/theme/index.ts
@@ -1,9 +1,13 @@
 import { colors } from './colors';
+import { fontFamilies } from './fontFamilies';
+import { fontSizes } from './fontSizes';
 import { shadow } from './shadow';
 
 export const theme = {
   colors,
   shadow,
+  fontFamilies,
+  fontSizes,
   gridUnit: 4,
   dividerSize: 1,
 };
