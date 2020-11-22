b26fa0108a9bd15546134e849d7cad24dc180099
Cyril Bonaccini <cyril.bonaccini@gmail.com>
2020-11-22 10:36:53 +0100

    add light status bar in CardNavigator

--- src/navigation/CardNavigator/CardNavigator.navigator.tsx
+++ src/navigation/CardNavigator/CardNavigator.navigator.tsx
@@ -1,6 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React, { FunctionComponent } from 'react';
-import { StatusBar } from 'react-native';
 import { theme } from '../../core/theme';
 import { colors } from '../../core/theme/colors';
 import { PlayCard } from '../../pages/PlayCard/PlayCard.page';
@@ -11,7 +10,6 @@ const Stack = createStackNavigator<CardNavigatorRouteParamsList>();
 
 export const CardNavigator: FunctionComponent = () => (
   <>
-    <StatusBar barStyle="dark-content" />
     <Stack.Navigator
       screenOptions={{
         headerStyle: {
